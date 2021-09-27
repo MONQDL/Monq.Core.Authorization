1bdcc6817ed94a3bff185708aca143c44376961b
Andrew <voknez@gmail.com>
2021-09-27 13:56:40 +0300

    🔖4.6.4. ⚡Добавлены расширения для работы с заголовком запроса.

---
+++ src/Monq.Core.Authorization/Extensions/HttpRequestHeaderExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Headers;
+
+namespace Monq.Core.Authorization.Extensions
+{
+    /// <summary>
+    /// Расширения для работы с заголовком запроса.
+    /// </summary>
+    public static class HttpRequestHeaderExtensions
+    {
+        /// <summary>
+        /// Заменить или добавить указанный ключ в заголовок запроса.
+        /// </summary>
+        /// <param name="httpRequestHeaders"></param>
+        /// <param name="name">Ключ.</param>
+        /// <param name="value">Значение.</param>
+        public static void TryUpdateWithoutValidation(this HttpRequestHeaders httpRequestHeaders, string name, string value)
+        {
+            if (httpRequestHeaders.Contains(name))
+                httpRequestHeaders.Remove(name);
+            httpRequestHeaders.TryAddWithoutValidation(name, value);
+        }
+
+        /// <summary>
+        /// Добавить указанный ключ в заголовок запроса, если такого еще нет.
+        /// </summary>
+        /// <param name="httpRequestHeaders"></param>
+        /// <param name="name">Ключ.</param>
+        /// <param name="value">Значение.</param>
+        public static void TryAddIfNotContain(this HttpRequestHeaders httpRequestHeaders, string name, string value)
+        {
+            if (httpRequestHeaders.Contains(name)) return;
+            httpRequestHeaders.TryAddWithoutValidation(name, value);
+        }
+    }
+}
